2015c0fe32479d38b8ef4f28beb2affa85c80b7a
Gurdip Sira <87333788+GurdipS5@users.noreply.github.com>
2025-12-22 01:35:58 +0000

    Update org_settngs.tf

--- src/org_settngs.tf
+++ src/org_settngs.tf
@@ -1,28 +1,29 @@
 resource "github_organization_settings" "org" {
-  billing_email                                                = "billing@example.com"
-  company                                                      = "My Company"
-  blog                                                         = "https://example.com"
-  email                                                        = "info@example.com"
-  twitter_username                                             = "mycompany"
-  location                                                     = "San Francisco"
-  name                                                         = "My Organization"
-  description                                                  = "We build cool stuff"
+  # Organization Information
+  billing_email    = var.org_billing_email
+  company          = var.org_company
+  blog             = var.org_blog
+  email            = var.org_email
+  twitter_username = var.org_twitter_username
+  location         = var.org_location
+  name             = var.org_name
+  description      = var.org_description
 
-  # Repository defaults
-  default_repository_permission                                = "read"
-  members_can_create_repositories                              = true
-  members_can_create_public_repositories                       = false
-  members_can_create_private_repositories                      = true
-  members_can_create_internal_repositories                     = false
-  members_can_create_pages                                     = true
-  members_can_fork_private_repositories                        = false
+  # Repository Defaults
+  default_repository_permission               = var.default_repository_permission
+  members_can_create_repositories             = var.members_can_create_repositories
+  members_can_create_public_repositories      = var.members_can_create_public_repositories
+  members_can_create_private_repositories     = var.members_can_create_private_repositories
+  members_can_create_internal_repositories    = var.members_can_create_internal_repositories
+  members_can_create_pages                    = var.members_can_create_pages
+  members_can_fork_private_repositories       = var.members_can_fork_private_repositories
 
   # Security
-  web_commit_signoff_required                                  = true
-  advanced_security_enabled_for_new_repositories               = true
-  dependabot_alerts_enabled_for_new_repositories               = true
-  dependabot_security_updates_enabled_for_new_repositories     = true
-  dependency_graph_enabled_for_new_repositories                = true
-  secret_scanning_enabled_for_new_repositories                 = true
-  secret_scanning_push_protection_enabled_for_new_repositories = true
+  web_commit_signoff_required                                  = var.web_commit_signoff_required
+  advanced_security_enabled_for_new_repositories               = var.advanced_security_enabled_for_new_repositories
+  dependabot_alerts_enabled_for_new_repositories               = var.dependabot_alerts_enabled_for_new_repositories
+  dependabot_security_updates_enabled_for_new_repositories     = var.dependabot_security_updates_enabled_for_new_repositories
+  dependency_graph_enabled_for_new_repositories                = var.dependency_graph_enabled_for_new_repositories
+  secret_scanning_enabled_for_new_repositories                 = var.secret_scanning_enabled_for_new_repositories
+  secret_scanning_push_protection_enabled_for_new_repositories = var.secret_scanning_push_protection_enabled_for_new_repositories
 }
